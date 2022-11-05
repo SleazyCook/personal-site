68e7365eb2d1a18603d147553edf853e231ad63c
Andrew Cook <andrewjcook93@gmail.com>
2022-11-05 16:32:00 -0500

    saving index.js

--- src/components/Games.js
+++ src/components/Games.js
@@ -11,9 +11,29 @@ const Games = () => {
         <Link to="/school">School</Link>
         <Link to="/work">Work</Link>
       </nav>
-      <p>
-        Games go here.
-      </p>
+      {/* Project Section */}
+      <div className="projects">
+        {/* PIXELATE */}
+        <div className="project">
+          <p>
+            Pixelate
+            <br />
+            Add rows and choose from 12 different colors to create your own pixel art. 
+            <br />
+            <a href="https://pixel8game.netlify.app" target="_blank">Click here to play now!</a>
+          </p>
+        </div>
+        {/* CONNECT FOUR */}
+        <div className="project">
+          <p>
+            Connect 4
+            <br />
+            Connect-Four is a tic-tac-toe-like two-player game in which players alternately place pieces on a vertical board 7 columns across and 6 rows high.
+            <br />
+            <a href="https://connect4bydrewford.netlify.app" target="_blank">Click here to play now!</a>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
