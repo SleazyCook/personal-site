5edb49b939b33f60401743b681cd88cd9b6c966c
Andrew Cook <andrewjcook93@gmail.com>
2022-11-05 17:04:49 -0500

    school entries added

--- src/components/School.js
+++ src/components/School.js
@@ -14,6 +14,29 @@ const School = () => {
       <p>
         School projects go here.
       </p>
+      {/* Project Section */}
+      <div className="projects">
+        {/* Drewford's List */}
+        <div className="project">
+          <p>
+            Drewford's list
+            <br />
+            Craigslist-clone made up a community of fellow cohorts practicing with front-end CRUD methods for a pre-built API.
+            <br />
+            <a href="https://drewfordslist.netlify.app" target="_blank">Click here to see this project</a>
+          </p>
+        </div>
+        {/* Puppy Bowl */}
+        <div className="project">
+          <p>
+            Puppy Bowl
+            <br />
+            My introduction to react.js, react-router, and front-end work with an API. 
+            <br />
+            <a href="https://puppybowlbydrewford.netlify.app/about" target="_blank">Click here to see this project</a>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
