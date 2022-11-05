082891f04a45e83efc8012d8edc716f6122d41cf
Andrew Cook <andrewjcook93@gmail.com>
2022-11-05 15:21:43 -0500

    fixed navbar visibility

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
-import {Outlet} from 'react-router-dom';
+import React from 'react';
 import Navbar from './Navbar';
+import {Outlet} from 'react-router-dom';
 
 const App = () => {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
-  <nav>
-    <Link to='/'>Home</Link>
-    <Link to='about'>About</Link>
-    <Link to='portfolio'>Portfolio</Link>
+  return (
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="about">About</Link>
+      <Link to="portfolio">Portfolio</Link>
   </nav>
+  )
+
 }
 
 export default Navbar;
