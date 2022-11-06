87e04000b42b652b801ce21e3d569779d128874e
Andrew Cook <andrewjcook93@gmail.com>
2022-11-06 00:01:34 -0500

    updated ideas for home and about pages. placeholders for certs

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,21 @@ const About = () => {
   return (
     <div>
       Everything about that crazy mofo Drew that you wanted to know so damn badly.
+      <ul>Add icons for:
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>Git</li>
+        <li>React.js</li>
+        <li>Express.js</li>
+        <li>Node.js</li>
+        <li>PostgreSQL</li>
+      </ul>
+      <p>CERTS FROM </p>
+      <p>Fullstack Academy</p>
+      <p>codecademy</p>
+      <p>udemy</p>
+      <p>freeCodeCamp</p>
     </div>
   )
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,20 +3,12 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
+      <br />
       "There is art and beauty and power in the primal images of fantasy" - Guillermo del Toro
       <br />
-      Basic resume statement
       <br />
-      <ul>Add icons for:
-        <li>HTML</li>
-        <li>CSS</li>
-        <li>JavaScript</li>
-        <li>Git</li>
-        <li>React.js</li>
-        <li>Express.js</li>
-        <li>Node.js</li>
-        <li>PostgreSQL</li>
-      </ul>
+      Fullstack Academy graduate  seeking first full-time position as a developer
+      <br />
     </div>
   )
 }
