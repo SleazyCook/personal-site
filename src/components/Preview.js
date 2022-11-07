4128b89919ed7655500ec33dc30f2995894172f3
Andrew Cook <andrewjcook93@gmail.com>
2022-11-07 10:32:38 -0600

    working on reworking vanilla into react

--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,67 +1,78 @@
-import React from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 
 const Preview = () => {
-  let currentScrollPostion = 0;
-  let scrollAmount = 320;
+  const [currentScrollPosition, setCurrentScrollPosition] = useState(0);
+  const [scrollAmount, setScrollAmount] = useState(320);
+  const sCont = useRef()
+  const hScroll = useRef()
+  const [maxScroll, setMaxScroll] = useState(0)
+  useEffect( () => {
+    setMaxScroll(-sCont.current.offsetWidth + hScroll.current.offsetWidth)
+  }, [sCont, hScroll])
+  // const [btnScrollLeft, setBtnScrollLeft] = useState(0);
 
-  const sCont = document.querySelector(".storys-container");
-  const hScroll = document.querySelector(".horizontal-scroll");
-  const btnScrollLeft = document.getElementById("btn-scroll-left");
-  const btnScrollRight = document.getElementById("btn-scroll-right");
+  // const sCont = document.querySelector(".storys-container");
+  // const hScroll = document.querySelector(".horizontal-scroll");
+  // const btnScrollLeft = document.getElementById("btn-scroll-left");
+  // const btnScrollRight = document.getElementById("btn-scroll-right");
 
-  btnScrollLeft.style.opacity= "0";
+  // btnScrollLeft.style.opacity= "0";
 
-  let maxScroll = -sCont.offsetWidth + hScroll.offsetWidth;
-  
-  function scrollHorizontally(val){
-    currentScrollPostion += (val * scrollAmount);
+  // const maxScroll = -sCont.offsetWidth + hScroll.offsetWidth;
+  console.log("sCont:", sCont)
+  console.log("hScroll:", hScroll)
+  function scrollHorizontally(event){
+    setCurrentScrollPosition(currentScrollPosition + (currentScrollPosition * scrollAmount)) 
 
-    if(currentScrollPostion >= 0){
-      currentScrollPostion = 0
-      btnScrollLeft.style.opacity= "0";
+    if(currentScrollPosition >= 0){
+      setCurrentScrollPosition(0)
+      event.target.style.opacity = 0;
     } else {
-      btnScrollLeft.style.opacity= "1";
+      event.target.style.opacity= 1;
     }
 
-    if(currentScrollPostion <= maxScroll) {
-      currentScrollPostion = maxScroll
-      btnScrollRight.style.opacity= "0";
+    if(currentScrollPosition <= maxScroll) {
+      setCurrentScrollPosition(maxScroll)
+      event.target.style.opacity = 0;
     } else {
-      btnScrollRight.style.opacity= "1";
+      event.target.style.opacity= 1;
     }
 
-    sCont.style.left = currentScrollPostion + "px"
+    sCont.current.style.left = currentScrollPosition + "px"
   }
+
   return(
     <div>
-      <body>
-        <div class="container">
-          <div class="horizontal-scroll">
+      <div className="body">
+        <div className="container">
+          <div className="horizontal-scroll" ref={hScroll}>
             {/* left button */}
-            <button class="btn-scroll" id="btn-scroll-left" onClick="scrollHorizontally(1)"><i class="fas fa-chevron-left"></i></button>
+            <button className="btn-scroll" id="btn-scroll-left" 
+            onClick={scrollHorizontally}><i className="fas fa-chevron-left"></i></button>
             {/* right button */}
-            <button class="btn-scroll" id="btn-scroll-right" onClick="scrollHorizontally(-1)"><i class="fas fa-chevron-right"></i></button>
+            <button className="btn-scroll" id="btn-scroll-right" 
+            onClick={scrollHorizontally}><i className="fas fa-chevron-right"></i></button>
 
-            <div class="storys-container">
-              <div class="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
-              <div class="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
-              <div class="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
-              <div class="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
-              <div class="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
-              <div class="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
-              <div class="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
-              <div class="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
-              <div class="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
-              <div class="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
-              <div class="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
-              <div class="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
-              <div class="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
-              <div class="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
-              <div class="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
+            <div className="storys-container" ref={sCont}>
+              <div className="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
+              <div className="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
+              <div className="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
+              <div className="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
+              <div className="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
+              <div className="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
+              <div className="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
+              <div className="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
+              <div className="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
+              <div className="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
+              <div className="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
+              <div className="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
+              <div className="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
+              <div className="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
+              <div className="story-circle"><img src="https://cdn.freecodecamp.org/curriculum/css-photo-gallery/1.jpg"/></div>
             </div>
           </div>
         </div>
-      </body>
+      </div>
     </div>
   )
 }
