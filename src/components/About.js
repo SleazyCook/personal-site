ab86c62c42800f4a164b256e60eb05a8aa429436
Andrew Cook <andrewjcook93@gmail.com>
2022-11-08 14:10:52 -0600

    flex and resizing for skill icons, basic style for About.section headers

--- src/components/About.js
+++ src/components/About.js
@@ -4,52 +4,79 @@ const About = () => {
   return (
     <div className="about-container">
 
-      Fullstack Academy graduate  seeking first full-time position as a developer
       <br />
-      <p id="skills">Skills</p>
-      <div className="languages">
+      <p className="text-center">
+      Fullstack Academy graduate  seeking first full-time position as a developer
+      </p>
+      <br />
+
+      <p id="skills" className='text-center text-2xl'>Skills</p>
+      <div className="flex flex-wrap justify-center">
         <div className="language">
-          HTML
-          <img alt="html icon" src="https://img.icons8.com/color/344/html-5--v1.png" height="100" width="100"></img>
+          <p className="text-center">
+            HTML
+          </p>
+          <img alt="html icon" src="https://img.icons8.com/color/344/html-5--v1.png" height="80" width="80"></img>
         </div>
 
         <div className="language">
-          CSS
-          <img alt="css icon" src="https://img.icons8.com/color/344/css3.png" height="100" width="100"></img>
+          <p className="text-center">
+            CSS
+          </p>
+          <img alt="css icon" src="https://img.icons8.com/color/344/css3.png" height="80" width="80"></img>
         </div>
 
         <div className="language">
-        JavaScript
-          <img alt="javascript icon" src="https://img.icons8.com/color/344/javascript--v2.png" height="100" width="100"></img>
+          <p className="text-center">
+            JavaScript
+          </p>
+          <img alt="javascript icon" src="https://img.icons8.com/color/344/javascript--v2.png" height="80" width="80"></img>
         </div>
 
         <div className="language">
-          Git
-          <img alt="git icon" src="https://img.icons8.com/color/344/git.png" height="100" width="100"></img>
+          <p className="text-center">
+            Git
+          </p>
+          <img alt="git icon" src="https://img.icons8.com/color/344/git.png" height="80" width="80"></img>
         </div>
 
         <div className="language">
-        React.js
-          <img alt="react icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" height="100" width="100"></img>
+          <p className="text-center">
+            Tailwind
+          </p>
+          <img alt="tailwind css icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2048px-Tailwind_CSS_Logo.svg.png" height="80" width="80"></img>
         </div>
 
         <div className="language">
-        Express.js
-          <img alt="express icon" src="https://www.mementotech.in/assets/images/icons/express.png" height="100" width="100"></img>
+          <p className="text-center">
+            React.js
+          </p>
+          <img alt="react icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" height="80" width="80"></img>
         </div>
 
         <div className="language">
-          Node.js
-          <img alt="node icon" src="https://cdn-icons-png.flaticon.com/128/5968/5968322.png" height="100" width="100"></img>
+          <p className="text-center">
+            Express.js
+          </p>
+          <img alt="express icon" src="https://www.mementotech.in/assets/images/icons/express.png" height="80" width="80"></img>
         </div>
 
         <div className="language">
-          PostgreSQL
-          <img alt="post gres Q L icon" src="https://www.vectorlogo.zone/logos/postgresql/postgresql-icon.svg" height="100" width="100"></img>
+          <p className="text-center">
+            Node.js
+          </p>
+          <img alt="node icon" src="https://cdn-icons-png.flaticon.com/128/5968/5968322.png" height="80" width="80"></img>
+        </div>
+
+        <div className="language">
+          <p className="text-center">
+            PostgreSQL
+          </p>
+          <img alt="post gres Q L icon" src="https://www.vectorlogo.zone/logos/postgresql/postgresql-icon.svg" height="80" width="80"></img>
         </div>
 
       </div>
-      <p>CERTS FROM </p>
+      <p className='text-center text-2xl'>Certifications</p>
       <p>Fullstack Academy</p>
       <p>codecademy</p>
       <p>udemy</p>
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -21,10 +21,12 @@ const Preview = () => {
       <br />
       <div className='relative flex items-center'>
         <MdChevronLeft className='opacity-50 cursor-pointer hover:opacity-100' onClick={sliderLeft} size={40} />
+        {/* scrollbar hide used install (npm install tailwind-scrollbar-hide) and plugin (tailwind.config.js) */}
         <div id='slider' className="w-full h-full overflow-x-scroll scroll scrollbar-hide whitespace-nowrap scroll-smooth">
           
           {previewData.map((item) => (
             <a href={item.link} target="_blank">
+              {/* Let's work on getting all images to uniform sizing with 4:3 ratio */}
                <img
               className='w-[220px] inline-block p-2 cursor-pointer hover:scale-105 ease-in-out duration-300' src={item.img} alt="/" />
             </a>
