f3d5a4e89c32bbb0209a07871d3f0930228e9b6a
Andrew Cook <andrewjcook93@gmail.com>
2022-11-08 14:24:02 -0600

    story section added to about

---
+++ src/components/Skills.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+const Skills = () => {
+  return (
+    <div>
+      <div className="flex flex-wrap justify-center">
+        <div className="language">
+          <p className="text-center">
+            HTML
+          </p>
+          <img alt="html icon" src="https://img.icons8.com/color/344/html-5--v1.png" height="80" width="80"></img>
+        </div>
+
+        <div className="language">
+          <p className="text-center">
+            CSS
+          </p>
+          <img alt="css icon" src="https://img.icons8.com/color/344/css3.png" height="80" width="80"></img>
+        </div>
+
+        <div className="language">
+          <p className="text-center">
+            JavaScript
+          </p>
+          <img alt="javascript icon" src="https://img.icons8.com/color/344/javascript--v2.png" height="80" width="80"></img>
+        </div>
+
+        <div className="language">
+          <p className="text-center">
+            Git
+          </p>
+          <img alt="git icon" src="https://img.icons8.com/color/344/git.png" height="80" width="80"></img>
+        </div>
+
+        <div className="language">
+          <p className="text-center">
+            Tailwind
+          </p>
+          <img alt="tailwind css icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/Tailwind_CSS_Logo.svg/2048px-Tailwind_CSS_Logo.svg.png" height="80" width="80"></img>
+        </div>
+
+        <div className="language">
+          <p className="text-center">
+            React.js
+          </p>
+          <img alt="react icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png" height="80" width="80"></img>
+        </div>
+
+        <div className="language">
+          <p className="text-center">
+            Express.js
+          </p>
+          <img alt="express icon" src="https://www.mementotech.in/assets/images/icons/express.png" height="80" width="80"></img>
+        </div>
+
+        <div className="language">
+          <p className="text-center">
+            Node.js
+          </p>
+          <img alt="node icon" src="https://cdn-icons-png.flaticon.com/128/5968/5968322.png" height="80" width="80"></img>
+        </div>
+
+        <div className="language">
+          <p className="text-center">
+            PostgreSQL
+          </p>
+          <img alt="post gres Q L icon" src="https://www.vectorlogo.zone/logos/postgresql/postgresql-icon.svg" height="80" width="80"></img>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Skills;
