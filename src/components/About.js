f8e19c795323bb5ac10678fdcee7c5d18cdc3a2b
Andrew Cook <andrewjcook93@gmail.com>
2022-11-09 01:00:07 -0600

    lorem ipsum added to about section

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,29 @@ import React from 'react';
 const About = () => {
   return (
     <div>
-      This is my story.
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu leo orci. Pellentesque mattis velit est, semper mattis enim euismod maximus. Quisque augue turpis, iaculis vel pellentesque nec, consequat nec ligula. Quisque sodales metus neque, et sollicitudin nibh lacinia a. Donec nisi justo, ullamcorper eu molestie at, pulvinar sed dolor. Nullam dignissim urna odio, et eleifend est posuere eget. Ut ultrices dictum hendrerit. Etiam eu metus id justo euismod cursus. Praesent maximus odio hendrerit, laoreet dolor sit amet, fringilla nisl. Curabitur quis elit auctor ante egestas condimentum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+
+        <br />
+        <br />
+
+        Cras blandit turpis vitae dolor pharetra, id suscipit velit laoreet. Donec rutrum urna nisi, nec cursus massa blandit bibendum. Nunc nunc dui, varius sed enim eu, ullamcorper mattis tortor. Suspendisse mollis bibendum lacus a lacinia. Cras venenatis consectetur nulla, et luctus mi pulvinar non. Ut dignissim ultrices purus non efficitur. Mauris urna quam, ornare eget est non, rutrum tristique turpis. In rutrum consequat purus efficitur venenatis. Vivamus porta pellentesque leo non condimentum. Quisque vitae dignissim dolor.
+
+        <br />
+        <br />
+
+        Aenean mi nisi, faucibus pulvinar consequat eu, fringilla a lectus. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum volutpat scelerisque tortor, vitae maximus eros. Donec fringilla vel purus vel blandit. Etiam rutrum venenatis dignissim. Integer pulvinar dignissim ante et rhoncus. Donec placerat tincidunt lobortis. Fusce posuere semper justo eu ultrices. Curabitur et rhoncus mi. Phasellus in purus lectus.
+        
+        <br />
+        <br />
+
+        Etiam ut elit ut ante fringilla gravida vitae eget massa. Sed lacinia luctus quam, non porta erat pellentesque convallis. Morbi condimentum gravida sapien at placerat. In malesuada et dolor nec consectetur. Duis tincidunt massa quis nisi mollis commodo. Vivamus euismod aliquam finibus. Duis dignissim odio ut metus egestas efficitur. Nullam ut ipsum quis metus efficitur egestas vel at massa. Sed at consequat ipsum. In in finibus tortor. Cras et ornare erat, vitae convallis enim. Mauris libero lectus, mollis auctor arcu eget, egestas facilisis elit. Duis commodo turpis sodales, aliquet elit cursus, posuere lorem. Vivamus malesuada elit urna, vel egestas ex commodo quis.
+
+        <br />
+        <br />
+
+        Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Donec malesuada nibh ut eros elementum mattis semper sit amet purus. Phasellus malesuada ut neque et dapibus. Vivamus sit amet viverra mi, vitae imperdiet metus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Proin porttitor efficitur sagittis. Proin sit amet urna et eros elementum feugiat nec at lacus. Cras lobortis congue lorem ut sagittis. Vivamus neque lorem, dignissim sed ex id, ornare tincidunt nisi. Aliquam pharetra dignissim nibh ut dignissim.
+      </p>
     </div>
   )
 }
