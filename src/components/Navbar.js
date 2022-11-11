b4e01b2ea7cb3d8f73936a12e1a218ee99e69073
Andrew Cook <andrewjcook93@gmail.com>
2022-11-10 23:34:10 -0600

    hamburger button added. dropdown dysfunctional

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,24 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
+import './navbar.css';
 
 const Navbar = () => {
-  const [isMenuClicked, setIsMenuClicked] = useState(false);
 
-  function handleOpenMenu () {
-    setIsMenuClicked(!isMenuClicked)
+  const [burger_class, setBurgerClass] = useState("burger-bar unclicked")
+  const [menu_class, setMenuClass] = useState("menu hidden")
+  const [isMenuClicked, setIsMenuClicked] = useState(false)
+
+  // toggle burger menu change
+  const updateMenu = () => {
+      if(!isMenuClicked) {
+          setBurgerClass("burger-bar clicked")
+          setMenuClass("menu visible")
+      }
+      else {
+          setBurgerClass("burger-bar unclicked")
+          setMenuClass("menu hidden")
+      }
+      setIsMenuClicked(!isMenuClicked)
   }
 
   // function Menu(e){
@@ -27,8 +40,29 @@ const Navbar = () => {
             <span className='text-lg overline'>Andrew Cook - Web Developer</span>
           </a></h1>
 
-          {/* hamburger menu button from Tailwind CSS
-          <div onClick={handleOpenMenu} className="lg:hidden flex flex-col justify-between max-h-[40px] min-h-[20px] mr-[25px] hover:cursor-pointer" >
+          {/* style={{width: '100%', height: '100vh'}} */}
+          <div id="box" className="lg:opacity-0 opacity-100">
+            <div id="burger-house">
+                <div className="burger-menu" onClick={updateMenu}>
+                    <div className={burger_class} ></div>
+                    <div className={burger_class} ></div>
+                    <div className={burger_class} ></div>
+                </div>
+            </div>
+
+            <div className={menu_class}>
+              <p>Hello</p>
+              <p>Hello</p>
+              <p>Hello</p>
+              <p>Hello</p>
+            </div>
+        </div>
+
+
+          {/* react-burger-menu by npm install */} 
+
+          {/* hamburger menu button from Tailwind CSS */}
+          {/* <div onClick={handleOpenMenu} className="lg:hidden flex flex-col justify-between max-h-[40px] min-h-[20px] mr-[25px] hover:cursor-pointer" >
             <div className='bg-white h-[3px] w-[40px] transition transform active:rotate-90' ></div>
             <div className='bg-white h-[3px] w-[40px]' ></div>
             <div className='bg-white h-[3px] w-[40px]' ></div>
