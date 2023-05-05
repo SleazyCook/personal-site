e7b3d49027ff9e73645840bc75b7cf4a817975f4
Andrew Cook <andrewjcook93@gmail.com>
2023-05-05 16:37:42 -0500

    dynamic website component, still working on new react icons

--- src/components/Websites.js
+++ src/components/Websites.js
@@ -2,6 +2,10 @@ import React from 'react';
 import Footer from './Footer';
 import {websiteData} from "./WebsiteData";
 
+import {FaUserAstronaut} from 'react-icons/fa';
+import {HiUserGroup} from 'react-icons/hi';
+import {BsGithub} from 'react-icons/bs'
+
 const Websites = () => {
   return (
     <div className="mx-10">
@@ -9,202 +13,28 @@ const Websites = () => {
       <br />
       <div className="max-w-[600px] mx-auto text-center">
 
-        {/* Marathon */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://watch-marathon.netlify.app" target="_blank">
-            Marathon | Streaming Service </a></p>
-          <p>
-            Browse through today's most popular movies and television shows.  Choose from any genre, or just browse by a certain vibe. Marathon is a streaming service primarily cloned from Netflix.com.  For legal reasons, we will only have trailers available for viewing. This project was pair programmed by myself and fellow Fullstack Academy alum <a href="https://madiwebb.netlify.app" targer="_blank">Madi Webb</a>. 
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">
-              React.js | SCSS | Express | PostgreSQL </p>
-            <br />
-          </p>
-        </div>
+        {websiteData.map((websiteObj) => {
+          return (
+            <div>
+              <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href={websiteObj.link} target="_blank">
+            {websiteObj.name} </a></p>
 
-        {/* Client Templates */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://tired-template.netlify.app" target="_blank">
-            Zach & Drew Make Websites </a></p>
-          <p>
-            Pair Programming project with developer Zach Power to create a variety of front end project templates including portfolios, product landing pages, and more.  This site serves as an outreach to potential freelance clients for a their unique and specific needs by showcasing a wide range of possibile webpages.
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">
-              React.js | CSS</p>
-            <br />
-          </p>
-        </div>
+            <p className="text-center">
+              {websiteObj.group ?  "Group project" : "Solo project"} | 
+              <span className="text-xl"><BsGithub /></span>
+              
+            </p>
 
-        {/* Cozy Cuddles */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://cozy-cuddles.netlify.app" target="_blank">
-            Cozy Cuddles | Adopt a Pet </a></p>
-          <p>
-            Solo project. Website to adopt pets.
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">
-              React.js | SCSS</p>
-            <br />
-          </p>
-        </div>
-
-        {/* Sunseekers */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://sunseekers.netlify.app/sunseekers" target="_blank">
-            Curse of Strahd: Plight of the Sunseekers </a></p>
-          <p>
-            Curse of Strahd is a popular campaign within the tabletop game Dungeons & Dragons.  Plight of the Sunseekers is my take on Curse of Strahd with my friends that play across the country on the virtual game board offered by Roll20.  This site contains my public notes for the game so far. 
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">
-              React.js | SCSS</p>
-            <br />
-          </p>
-        </div>
-
-        {/* Natours */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://natours-by-drewford.netlify.app" target="_blank">
-            Natours | Nature Tours </a></p>
-          <p>
-            Static site using only HTML and SASS originally created by Jonas Schedtmann for his Advanced CSS and SASS course for Udemy. Natours is a landing page for a fictional company that offers a variety of natours tours.  Follow through a page a of various animations to see features, reviews, and book your first tour. 
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">
-              HTML | SASS</p>
-            <br />
-          </p>
-        </div>
-
-        {/* Trillo */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://trillo-by-drewford.netlify.app" target="_blank">
-            trillo | Hotel Booking </a></p>
-          <p>
-            Static site using only HTML, SASS, and Flexbox originally created by Jonas Schedtmann for his Advanced CSS and SASS course for Udemy. trillo is a landing page for a fictional company that offers a variety of travel bookings.  Follow through a page a of various animations to see features, reviews, and book your first trip.
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">
-              HTML | SASS</p>
-            <br />
-          </p>
-        </div>
-
-        {/* Trillo */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://trillo-by-drewford.netlify.app" target="_blank">
-            nexter | Your Own Home </a></p>
-          <p>
-            Static site using only HTML, SASS, and CSS Grid originally created by Jonas Schedtmann for his Advanced CSS and SASS course for Udemy. nexter is a landing page for a fictional company that offers a variety of international offers for real estate.  Follow through a page a of various animations to see features, reviews, and find your own home.
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">
-              HTML | SASS</p>
-            <br />
-          </p>
-        </div>
-
-        {/* Poster Children */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://poster-children.netlify.app" target="_blank">
-            Poster Children</a></p>
-          <p>
-            Capstone Project from Fullstack Academy Bootcamp. Collaboration with a small group of cohorts to create a fake, but functional E-Commerce website for buying posters from movies, musical artists, and videogames. Register and create an account. Add items to your cart and store addresses and credit card information for checkout. Leave reviews on shop items. 
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">
-              React.js | CSS | Express.js | PostgreSQL</p>
-            <br />
-          </p>
-        </div>
-
-        {/* Dwayne the Shell's Travel Journal */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://dwayne-the-shell.netlify.app" target="_blank">
-            Dwayne's Travel Journal</a></p>
-          <p>
-            Player notes for a D&D campaign.  Character is Dwayne the Shell Johnson, a small humanoid turtle merchant who moonlights as a mercenary. This site contains character art, notes from game sessions, and invtenory items. There is also a hidden backstory page.
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">
-              React.js | SCSS </p>
-            <br />
-          </p>
-        </div>
-
-        {/* Delavega Autoplex */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://delavegaautoplex.netlify.app" target="_blank">
-            Delavega Autoplex</a></p>
-          <p>
-            Business site for car dealership in McAllen, TX. Learn about Delavega Autoplex's local history and inventory of cars, SUVs, and trucks. Read testimonials from verified purchasers, reach out directly to the owners, and manage your payments and payment history.
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">
-              React.js | CSS | Express.js | PostgreSQL</p>
-            <br />
-          </p>
-        </div>
-
-        {/* Victor Alberto Photography */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://victoralbertophotography.netlify.app/" target="_blank">
-            Victor Alberto Photography</a></p>
-          <p>
-            Photoraphy and Videography services by Victor Alberto Torres. Check out a gallery of previous works on weddings, engagements, graduation, landscapes, real estate, creative, wildlife, and nature. Book a session today or just order some digital or physical prints. 
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">
-              React.js | CSS | Material-UI</p>
-            <br />
-          </p>
-        </div>
-
-        {/* Movie Mayhem */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://moviemayhem.netlify.app" target="_blank">
-            Movie Mayhem</a></p>
-          <p>
-            Join a community of movie and television lovers at Movie Mayhem. Similar to the social film discovery website Letterboxd, discover and track your watchlist and participate in watchparties and discussions posted throughout the year. 
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">
-              React.js | CSS | Express.js | PostgreSQL</p>
-            <br />
-          </p>
-        </div>
-
-        {/* Beyond the Book */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://beyondthebook.netlify.app" target="_blank">
-            Beyond the Book</a></p>
-          <p>
-            Join a community of book lovers at Beyond the Book. Similar to the social reading discovery website Goodreads, discover and track your reading list and participate in bookclubs and discussions posted throughout the year.
-            <br />
-            <p className="text-center text-sm transition-all ease-in text-gray-500 overline">
-              React.js | CSS | Express.js | PostgreSQL</p>
-            <br />
-          </p>
-        </div>
-
-        {/* Drewford's List */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://drewfordslist.netlify.app" target="_blank">
-            Drewford's List</a></p>
-          <p>
-            A school project experimenting with Front End fetch requests to create, read, update, and destroy data from a premade database. This website was eventually led to the Homestead List, seen above, which is used by a community in Houston, TX.
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">
-              React.js | CSS</p>
-            <br />
-          </p>
-        </div>
-
-        {/* Homestead List */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://homesteadlist.netlify.app" target="_blank">
-            Homestead list: <span className="italic no-underline">Coming Soon!</span></a></p>
-          <p>
-            Buy, Sell, or Trade with neighbors you already know in the Houston Gardens area. A Craigslist-clone specifically for a neighborhood in Houston, Texas.
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">
-              React.js | CSS</p>
-            <br />
+            <p>{websiteObj.description}
+              <br />
+              <p className="text-center text-sm text-gray-500 overline">
+                {websiteObj.stack} </p>
+              <br />
+            </p>
             
-          </p>
-        </div>
-
+            </div>
+          )
+        })}
 
       </div>
 
