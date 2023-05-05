4764aee5fded0584c76415396baea17df4c32d8f
Andrew Cook <andrewjcook93@gmail.com>
2023-05-05 11:53:23 -0500

    add website data for eventual map

---
+++ src/components/WebsiteData.js
@@ -0,0 +1,101 @@
+export const websiteData = [
+
+  {
+    name: "Marathon | Streaming Service",
+    link: "https://watch-marathon.netlify.app/",
+    github: "https://github.com/orgs/React-Royalty/repositories",
+    description: "Browse through today's most popular movies and television shows. Choose from any genre, or just browse by a certain vibe. Marathon is a streaming service primarily cloned from Netflix.com. For legal reasons, we will only have trailers available for viewing. This project was pair programmed by myself and fellow Fullstack Academy alum Madi Webb.",
+    group: true,
+    stack: "React.js | SCSS | Express | PostgreSQL | API | Netlify | Render"
+  },
+
+  {
+    name: "Zach and Drew Make Websites",
+    link: "https://come-at-us.netlify.app",
+    github: "https://github.com/orgs/Tired-Poor/repositories",
+    description: "Pair Programming project with developer Zach Power to create a variety of front end project templates including portfolios, product landing pages, and more. This site serves as an outreach to potential freelance clients for a their unique and specific needs by showcasing a wide range of possibile webpages.",
+    group: true,
+    stack: "React.js | CSS | SCSS | Netlify"
+  },
+
+  {
+    name: "Cozy Cuddles | Adopt a Pet",
+    link: "https://cozy-cuddles.netlify.app",
+    github: "https://github.com/SleazyCook/adoption",
+    description: "Landing page for a fictional pet adoption agency. This project uses CSS Grid to create a fully responsive web application to market this agency including their services offered and values. Browse through available pets and get to know them by name, age, and breed. At the bottom of the web page, there is a beautiful form using custom radio buttons to submit an inquiry for adoption, volunterring, and more.",
+    group: false,
+    stack: "React.js | SCSS | Netlify"
+  },
+
+  {
+    name: "Curse of Strahd: Plight of the Sunseekers",
+    link: "https://sunseekers.netlify.app",
+    github: "https://github.com/SleazyCook/sunseekers",
+    description: "Curse of Strahd is a popular campaign within the tabletop game Dungeons & Dragons. Plight of the Sunseekers is my take on Curse of Strahd with my friends that play across the country on the virtual game board offered by Roll20. This site contains my public notes for the game so far.",
+    group: false,
+    stack: "React.js | SCSS | Netlify"
+  },
+
+  {
+    name: "Natours | Nature Tours",
+    link: "https://natours-by-drewford.netlify.app",
+    github: "https://github.com/SleazyCook/natours",
+    description: "Static site using only HTML and SASS originally created by Jonas Schedtmann for his Advanced CSS and SASS course for Udemy. Natours is a landing page for a fictional company that offers a variety of natours tours. Follow through a page a of various animations to see features, reviews, and book your first tour.",
+    group: false,
+    stack: "HTML | SASS | Netlify"
+  },
+
+  {
+    name: "trillo | Hotel Booking",
+    link: "https://trillo-by-drewford.netlify.app",
+    github: "https://github.com/SleazyCook/trillo",
+    description:  "Static site using only HTML, SASS, and Flexbox originally created by Jonas Schedtmann for his Advanced CSS and SASS course for Udemy. trillo is a landing page for a fictional company that offers a variety of travel bookings. Follow through a page a of various animations to see features, reviews, and book your first trip.",
+    group: false,
+    stack: "HTML | SASS | Netlify"
+  },
+
+  {
+    name: "nexter | Your Own Home",
+    link: "https://nexter-by-drewford.netlify.app",
+    github: "https://github.com/SleazyCook/Nexter",
+    description: "Static site using only HTML, SASS, and CSS Grid originally created by Jonas Schedtmann for his Advanced CSS and SASS course for Udemy. nexter is a landing page for a fictional company that offers a variety of international offers for real estate. Follow through a page a of various animations to see features, reviews, and find your own home.",
+    group: false,
+    stack: "HTML | SASS | Netlify"
+  },
+
+  {
+    name: "Poster Children | E-Commerce Web Application",
+    link: "https://poster-children.netlify.app",
+    github: "https://github.com/2209-Poster-Children",
+    description: "Capstone Project from Fullstack Academy Bootcamp. Collaboration with a small group of cohorts to create a fake, but functional E-Commerce website for buying posters from movies, musical artists, and videogames. Register and create an account. Add items to your cart and store addresses and credit card information for checkout. Leave reviews on shop items.",
+    group: true,
+    stack: "React.js | CSS | Express.js | PostgreSQL | API | Stripe | Netlify | Render"
+  },
+
+  {
+    name: "Dwayne's Travel Journal | D&D Companion",
+    link: "https://dwayne-the-shell.netlify.app",
+    github: "https://github.com/SleazyCook/dwayne-the-shell",
+    description: "Player notes for a D&D campaign. Character is Dwayne the Shell Johnson, a small humanoid turtle merchant who moonlights as a mercenary. This site contains character art, notes from game sessions, and invtenory items. There is also a hidden backstory page.",
+    group: false,
+    stack: "React.js | SCSS | Netlify"
+  },
+
+  {
+    name: "Delavega Autoplex",
+    link: "https://delavegaautoplex.netlify.app",
+    github: "https://github.com/SleazyCook/autoplex",
+    description: "Business site for car dealership in McAllen, TX. Learn about Delavega Autoplex's local history and inventory of cars, SUVs, and trucks. Read testimonials from verified purchasers, reach out directly to the owners, and manage your payments and payment history.",
+    group: false,
+    stack: "React.js | CSS | Express.js | PostgreSQL | API | Netlify"
+  },
+
+  {
+    name: "Drewford's List",
+    link: "https://drewfordslist.netlify.app",
+    github: "https://github.com/SleazyCook/project_05",
+    description: "A school project experimenting with Front End fetch requests to create, read, update, and destroy data from a premade database.",
+    group: false,
+    stack: "React.js | CSS"
+  }
+]
--- src/components/Websites.js
+++ src/components/Websites.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Footer from './Footer';
+import {websiteData} from "./WebsiteData";
 
 const Websites = () => {
   return (
