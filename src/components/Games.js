b041016db8156ccf0ee8e9aa4d75005b9203385f
Andrew Cook <andrewjcook93@gmail.com>
2023-05-06 10:36:12 -0500

    add dynamic game component

--- src/components/Games.js
+++ src/components/Games.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Footer from './Footer';
+import {gamesData} from './GamesData';
 
 const Games = () => {
   return (
@@ -9,99 +10,33 @@ const Games = () => {
       {/* Project Section */}
       <div className="max-w-[600px] mx-auto text-center">
 
-        {/* Cards */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://card-slam.netlify.app" target="_blank">
-            Card Games</a></p>
-          <p>
-            Group project: sleek website hosting several cards games including Hearts and Spades. This site aims to host a variety of game options and hopefully the ability to host mulitplayer games across different computers.
+        {gamesData.map((gameObj) => {
+          return (
+            <div>
+              <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href={gameObj.link} target="_blank">
+            {gameObj.name}</a></p>
+
+            <p className="text-center">
+              {gameObj.group ?  <>  Group project </> : <>Solo project</>}
+              &nbsp;| &nbsp;
+              <a 
+                href={gameObj.github} target="_blank"
+                className="transition-all ease-in hover:text-gray-500">
+                  GitHub</a>
+              {/* <span className="text-xl"><BsGithub /></span> */}
+            </p>
+
+            <p>{gameObj.description}
             <br />
-            <p className="text-center text-sm text-gray-500 overline">React.js | SCSS </p>
+            <p className="text-center text-sm text-gray-500 overline">
+              {gameObj.stack}
+            </p>
             <br />
           </p>
-        </div>
 
-        {/* Sudoku */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://sudoku-slam.netlify.app" target="_blank">
-            Sudoku</a></p>
-          <p>
-            Group project: tranquil sudoku webpage. Sudoku is a logic-based, combinatorial number placement puzzle. In classic Sudoku, the objective is to fill a 9 x 9 grid with digits so that each column, row, and each of the nine 3 x 3 subgrids contain digits 1 to 9.
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">React.js | SCSS </p>
-            <br />
-          </p>
-        </div>
-
-        {/* DEATH HOUSE */}
-          <div>
-            <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://death-house.netlify.app" target="_blank">
-              Curse of Strahd: Durst Manor</a></p>
-          <p>
-            A multiple choice adventure game based on the introduction to the popular Curse of Strahd campagin setting from Dungeons and Dragons. To start you will choose a character ability: Strength, Dexterity, Charisma, or Luck. Use your ability to navigate through Durst Manor to uncover the truth and get out alive!
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">HTML | CSS | JavaScript</p>
-            <br />
-          </p>
-        </div>
-
-        <br />
-
-        {/* POKEMON PICKER */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://gotta-pick-em-all.netlify.app" target="_blank">
-            Pokemon Picker</a></p>
-          <p>
-            Team project inspired by either.io, Pokemon Picker offers you the chance to rank your favorite pokemon by choosing between two randomly selected pokemon at a time.  Choose which generation you are interested in ranking and view your statistics based on wins, type, and generation. 
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">React.js | CSS </p>
-            <br />
-          </p>
-        </div>
-
-        <br />
-        {/* CONNECT FOUR */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://connect4bydrewford.netlify.app" target="_blank">
-            Connect 4</a></p>
-          <p>
-            Connect-Four is a tic-tac-toe-like two-player game in which players alternately place pieces on a vertical board 7 columns across and 6 rows high.
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">HTML | CSS | JavaScript</p>
-            <br />
-          </p>
-        </div>
-
-        <br />
-
-        {/* PIXELATE */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://pixel8game.netlify.app" target="_blank">
-            Pixelate</a></p>
-          <p>
-          Add rows and choose from 12 different colors to create your own pixel art. 
-          <br />
-          <p className="text-center text-sm text-gray-500 overline">HTML | CSS | JavaScript</p>
-          <br />
-          
-          </p>
-        </div>
-
-        <br />
-
-        {/* Puppy Clicker */}
-        <div>
-          <p className="text-center text-xl transition-all ease-in hover:text-gray-500"><a href="https://puppy-clicker.netlify.app" target="_blank">
-            Puppy Clicker</a></p>
-          <p>
-            A cute incremental, or clicker, game with a puppy theme! Click or tap the puppy icon to start building resources and use those resources to purchase passive producers until you have a ridiculous amount of puppies!
-            <br />
-            <p className="text-center text-sm text-gray-500 overline">HTML | CSS | JavaScript</p>
-            <br />
-            
-          </p>
-        </div>
-
+            </div>
+          )
+        })}
 
       </div>
       <br />
