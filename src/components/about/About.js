fbfacf59b82ecb36c680310a9bbb373faf98f0a5
Andrew Cook <andrewjcook93@gmail.com>
2023-05-06 12:07:16 -0500

    clean up My Story

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -14,23 +14,23 @@ const About = () => {
         <br />
         <br />
 
-        Despite a passion for social sciences and higher education, I didn't graduate. I leaned into work and found myself working long hours in customer service and overnight clerical positions. I eventually found my way into healthcare management through Brave Hearts Children Center, a residential treatment center for children with intense and violent behaviors. I was passionate about working with these kids and learning the ins and outs of vairous levels of management while supervising up to 65 residents and 40 staff daily. When the center shut down, I continued my work as a manager at a covid testing site in The Woodlands, Texas for Curative. While the site was smaller, we were taking on close to 1,000 patients a day during the Omicron spike. As interest in testing declined exponentially, I found my success with a company cut short once again by another lay off. 
+        Despite a passion for social sciences and higher education, I didn't graduate. I leaned into work and found myself working long hours in customer service and overnight clerical positions. I eventually found my way into healthcare management through Brave Hearts Children Center, a residential treatment center for children age 0-17 with intense and violent behaviors. I was passionate about working with these kids and learning the ins and outs of vairous levels of management while supervising up to 65 residents and 40 staff daily. When the center shut down, I continued my work as a manager at a covid testing site in The Woodlands, Texas for Curative. While the site was smaller, we were taking on close to 1,000 patients a day during the Omicron spike. As interest in testing declined exponentially, I found my success with a company cut short once again by another lay off. 
 
         <br />
         <br />
 
-        With a decade of experience and softskills in management and customer service, I decided it was time for some hard skills. That's when I discovered Fullstack Academy and their Full-Stack Web Development Bootcamp. I poured all of my time and attention into that experience for the full twelve weeks. Through tough weekly projects, I discovered an entirely new passion for programming. I would code in my freetime and even wake up in the middle of the night suddenly with the solution to the bugs in my code from earlier in the day. By graduation, I was proficient in HTML, CSS, JavaScript, React, Node, Express, PostgreSQL, SQL, API, Git, GitHub, Netlify, and Render.
+        With a decade of experience and softskills in management and customer service, I decided it was time to develop some hard skills. That's when I discovered Fullstack Academy and their Full-Stack Web Development Bootcamp. I poured all of my time and attention into that experience for the full twelve weeks. Through tough weekly projects, I discovered an entirely new passion for programming. I would code in my freetime and even wake up in the middle of the night suddenly with the solution to the bugs in my code from earlier in the day. By graduation, I was proficient in HTML, CSS, JavaScript, React, Node, Express, PostgreSQL, SQL, API, Git, GitHub, Netlify, and Render.
         
         <br />
         <br />
 
-        After graduation I decided to stay home as long as I could make it work financially to keep working on my skills. I gained a higher understanding of the technology I was already familiar with like React and JavaScript. I took on new technologies like Tailwind CSS, Python, Material UI, CSS Grid, SCSS, Vue, and TypeScript. I worked on projects solo and made sure to keep in touch with my fellow Fullstack alums to work on group projects on a weekly basis. Some of my highlights in the time include a "web slam" project with Ian Sorensen where we made something new every week for a month or template website with Zach Power to meet with potential clients. My best passion so far is Marathon, a streaming site built with Madilyn Webb.
+        After graduation I decided to stay home as long as I could make it work financially to keep working on my skills. I gained a higher understanding of the technology I was already familiar with like React and JavaScript. I took on new technologies like Tailwind CSS, Python, Material UI, CSS Grid, SCSS, Vue, and TypeScript. I worked on projects solo and made sure to keep in touch with my fellow Fullstack alums to work on group projects on a weekly basis. Some of my highlights from the time include a "web slam" project with Ian Sorensen where we made something new every week for a month or template website with Zach Power to meet with potential clients. My best passion project so far is Marathon, a streaming site built with Madilyn Webb.
 
         <br />
         <br />
 
         
-        These days I'm applying for software engineering roles across the country, but I still find the time to work on group projects and work on algorithm practice regularly. 
+        These days I'm expanding my network and aggressively applying for software engineering roles across the country, but I still find the time to work on group projects and work on algorithm practice regularly. 
 
         <br />
         <br />
