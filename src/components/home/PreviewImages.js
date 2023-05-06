47caf811db24af2e27459da0ce9469aaf81571ff
Andrew Cook <andrewjcook93@gmail.com>
2023-05-06 12:26:28 -0500

    clean up PreviewImages. rm unused indexes

--- src/components/home/PreviewImages.js
+++ src/components/home/PreviewImages.js
@@ -68,10 +68,7 @@ export const previewData = [
     link: "https://delavegaautoplex.netlify.app"
   },
 
-
-
   {
-    id: 3,
     name: "Curse of Strahd: Durst Manor",
     img: "https://i.imgur.com/q1UVNrk.png",
     link: "https://death-house.netlify.app"
@@ -90,68 +87,66 @@ export const previewData = [
   },
 
   {
-    id: 20,
     name: "Victor Alberto Photography",
     img: "https://i.imgur.com/AbNB2uF.png",
     link: "https://victoralbertophotography.netlify.app/"
   },
 
   {
-    id: 4,
     name: "Drewford's List",    
     img: "https://i.imgur.com/mt4Idyv.png",
     link: "https://drewfordslist.netlify.app"
   },
+
   {
-    id: 5,
     name: "Connect 4",
     img: "https://i.imgur.com/X1ltYe2.png",
     link: "https://connect4bydrewford.netlify.app"
   },
+
   {
-    id: 1,
     name: "Puppy Clicker",
     img: "https://i.imgur.com/L8uk5oR.png?1",
     link: "https://puppy-clicker.netlify.app"
   },
+
   {
-    id: 6,
     name: "Pixelate",
     img: "https://i.imgur.com/GJcAJt4.png",
     link: "https://pixel8game.netlify.app"
   },
+
   {
-    id: 100,
     name: "FitnessTracker",
     img: "https://i.imgur.com/x2VCp0B.png",
     link: "https://fitness-by-prestige-worldwide.netlify.app"
   },
+
   {
-    id: 7,
     name: "Wizard News",
     img: "https://i.imgur.com/kXTLBtf.png",
     link: "https://wizardnewswithpnd.fly.dev"
   },
+
   {
-    id: 8,
     name: "Movie Mayhem",
     img: "https://i.imgur.com/9zFb5XE.png",
     link: "https://moviemayhem.netlify.app"
   },
+
   {
-    id: 9,
     name: "Beyond the Book",
     img: "https://i.imgur.com/8HhXvXE.png",
     link: "https://beyondthebook.netlify.app"
   },
+
   {
-    id: 10,
     name: "Homestead List",
     img: "https://i.imgur.com/uOYKUJu.png",
     link: "https://homesteadlist.netlify.app"
   },
+
   {
-    id: 11,
     name: "Puppy Bowl",
     img: "https://i.imgur.com/f9opkZT.png",
     link: "https://puppybowlbydrewford.netlify.app"
