05d584435cb66af5784f14d4c2562323d0ed9b49
Andrew Cook <andrewjcook93@gmail.com>
2023-05-06 10:43:27 -0500

    add certsData

---
+++ src/components/CertsData.js
@@ -0,0 +1,39 @@
+export const certsData = [
+
+  {
+    name: "Full-Stack Web Development Bootcamp",
+    type: "Certificate of Completion",
+    issue: "December 9th, 2022",
+    issuer: "University of Texas at Dallas",
+    detail: "Powered by Fullstack Academy"
+  },
+
+  {
+    name: "Advanced CSS & SASS",
+    type: "Certificate of Completion",
+    issue: "March 5th, 2023",
+    issuer: "Udemy"
+  },
+
+  {
+    name: "The Git & GitHub Bootcamp",
+    type: "Certificate of Completion",
+    issue: "Feburary 14th, 2023",
+    issuer: "Udemy"
+  },
+
+  {
+    name: "Full-Stack Software Engineer Career Path",
+    type: "Certificate of Completion",
+    issue: "November 15th, 2022",
+    issuer: "Codecademy"
+  },
+
+  {
+    name: "Responsive Web Design",
+    type: "Certificate of Completion",
+    issue: "November 22nd, 2022",
+    issuer: "freeCodeCamp"
+  }
+
+]
