2fdd7db8590f3a6629c53cd1d10cbe3db344f13e
Andrew Cook <andrewjcook93@gmail.com>
2023-05-06 11:28:22 -0500

    dynamic certs component

--- src/components/education/Certs.js
+++ src/components/education/Certs.js
@@ -1,56 +1,27 @@
 import React from 'react';
+import {certsData} from './CertsData';
 
 const Certs = () => {
   return (
     <div >
       <ul className="text-center">
-        <li>
-          <p><b>Full-Stack Web Development Bootcamp</b></p>
-          <p>Certificate of Completion</p>
-          <p>Issued: December 9th, 2022</p>
-          <p>University of Texas at Dallas</p>
-          <p>Powered by Fullstack Academy</p>
-        </li>
 
-        <br />
 
-        <li>
-          <p><b>Advanced CSS & SASS</b></p>
-          <p>Certificate of Completion</p>
-          <p>Issued: March 5th, 2023</p>
-          <p>Udemy</p>
-        </li>
+        {certsData.map((certsObj) => {
+          return (
+            <>
+              <li>
+                <p><b>{certsObj.name}</b></p>
+                <p>{certsObj.type}</p>
+                <p>Issued: {certsObj.issue}</p>
+                <p>{certsObj.issuer}</p>
+                <p>{certsObj.detail}</p>
+              </li>
 
-        <br />
-
-        <li>
-          <p><b>The Git & GitHub Bootcamp</b></p>
-          <p>Certificate of Completion</p>
-          <p>Issued: February 14, 2023</p>
-          <p>Udemy</p>
-        </li>
-
-        <br />
-
-        <li>
-          <p><b>Full-Stack Software Engineer Career Path</b></p>
-          <p>Certificate of Completion</p>
-          <p>Issued: November 15th, 2022</p>
-          <p>Codecademy</p>
-        </li>
-
-        <br />
-
-        <li>
-          <p><b>Responsive Web Design</b></p>
-          <p>Certificate of Completion</p>
-          <p>Issued: November, 22, 2022</p>
-          <p>freeCodeCamp</p>
-        </li>
-
-        
-        <br />
-
+              <br />
+            </>
+          )
+        })}
 
         <p className='text-center text-2xl underline'>
           Code Wars
